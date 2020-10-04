ece242e378fb21e73a70b2d7b6d3813d61a3ec06
bryanhorus <bryan.alva132@gmail.com>
2020-10-03 20:00:11 -0500

    creacion de servicios para la orden

--- src/main/java/com/vtsoft/vts/repository/OrdenRepository.java
+++ src/main/java/com/vtsoft/vts/repository/OrdenRepository.java
@@ -9,10 +9,10 @@ public interface OrdenRepository  extends JpaRepository<Orden , Long> {
 
     @Query(nativeQuery = true, value = "SELECT " +
             "id_Orden, " +
-            "total, " +
-            "fecha_v, " +
-            "fk_orden_id " +
-            "FROM venta " +
+            "estado, " +
+            "fecha, " +
+            "fk_producto_id " +
+            "FROM orden " +
             "WHERE id_Orden =:idOrden")
     Orden findByIdOrden(@Param(value = "idOrden") Long idOrden);
 
