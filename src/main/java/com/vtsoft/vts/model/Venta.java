8abcc0f7c0012e54882039d01efc36d45a2ade38
bryanhorus <bryan.alva132@gmail.com>
2020-10-27 15:39:15 -0500

    correcciones venta

--- src/main/java/com/vtsoft/vts/model/Venta.java
+++ src/main/java/com/vtsoft/vts/model/Venta.java
@@ -1,5 +1,73 @@
 package com.vtsoft.vts.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Entity
+@Table(name = "venta")
 public class Venta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venta")
+    private Long idVenta;
+
+    @OneToOne
+    @JoinColumn(name = "fk_orden_id",referencedColumnName = "id_orden")
+    private Orden orden;
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "fecha_v")
+    private Instant fecha;
+
+    public Venta() {
+
+    }
+
+    public Long getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Long idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Orden getOrden() {
+        return orden;
+    }
+
+    public void setOrden(Orden orden) {
+        this.orden = orden;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public Instant getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Instant fecha) {
+        this.fecha = fecha;
+    }
+
+    /*public Double getTotalv(){
+
+        return total += orden.calcularImporte();
+    }*/
 }
+
